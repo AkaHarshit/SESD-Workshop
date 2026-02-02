5d4c44da4932340d3083a2545a159ac9e58ec5c0
AkaHarshit <AkaHarshit@users.noreply.github.com>
2026-02-02 19:57:00 +0530

    Remove all files

--- backend/src/repositories/IUserRepository.ts
+++
@@ -1,8 +0,0 @@
-import { IUserDocument } from '../models/User';
-import { RegisterUserDTO } from '../types/dto';
-
-export interface IUserRepository {
-  create(data: RegisterUserDTO): Promise<IUserDocument>;
-  findByEmail(email: string): Promise<IUserDocument | null>;
-  findById(id: string): Promise<IUserDocument | null>;
-}
